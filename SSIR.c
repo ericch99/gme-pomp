c4e44e0155e713ef6a72ed62f518afa18f38c26b
Eric Chen <ericch@umich.edu>
2021-04-18 22:12:35 -0400

    done!

--- SSIR.c
+++ SSIR.c
@@ -1,8 +1,8 @@
 /* pomp C snippet file: SSIR */
-/* Time: 2021-04-18 13:50:26.110 -0500 */
+/* Time: 2021-04-18 22:09:42.104 -0400 */
 /* Salt: F5C3D9D05BFD17AABD39D608 */
 
-#include <pomp.h>
+#include <C:/Users/HP/Documents/R/win-library/4.0/pomp/include/pomp.h>
 #include <R_ext/Rdynload.h>
 
  
